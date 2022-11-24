b91da0f682e162233e3236b6e234db08e0c7a07f
Emmanuel <emmanuel.lebaramo@gmail.com>
2022-11-24 01:07:18 +0100

    BtnMeh added in script

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,1 +1,31 @@
 console.log(document.querySelector('body'))
+
+const cardsElements = document.querySelectorAll('.cards')
+const btnMeh = document.querySelector('.bxs-meh-alt')
+const btnHeart = document.querySelector('.bxs-heart')
+
+let curentActive = 0
+
+const heartFunction = () => {
+    const lastElement = cardsElements.length - 1
+    const nextActive = curentActive == lastElement ? 0 : curentActive + 1
+    
+    cardsElements[curentActive].setAttribute('statut', 'not-active')
+    cardsElements[nextActive].setAttribute('statut', 'active')
+
+    curentActive == lastElement ? curentActive = 0 : curentActive++
+}
+
+const mehFunction = () => {
+    const lastElement = cardsElements.length - 1
+    const nextActive = curentActive == 0 ? lastElement : curentActive - 1
+    
+    cardsElements[curentActive].setAttribute('statut', 'not-active')
+    cardsElements[nextActive].setAttribute('statut', 'active')
+
+    curentActive == 0 ? curentActive = lastElement : curentActive--
+}
+
+btnHeart.addEventListener('click', heartFunction)
+
+btnMeh.addEventListener('click', mehFunction)
